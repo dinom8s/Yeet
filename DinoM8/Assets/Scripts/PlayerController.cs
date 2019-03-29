836412d0e109f64ab2e4b1bc2c6c9923272adb77
Kalla01 <Kalla01@users.noreply.github.com>
2019-03-29 15:14:32 +0100

    kggjkw ad
    j, d

--- DinoM8/Assets/Scripts/PlayerController.cs
+++ DinoM8/Assets/Scripts/PlayerController.cs
@@ -2,31 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// behöver lägga till en character controller på player för att det ska funka sedan dra din spelare in i controller 
+
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
-
-    public float speed = 50f;
-    public CharacterController controller;
-
     public float gravityScale;
-    private Vector3 moveDirection;
+    public float speed = 200f;
+    public float acceleration;
+
+    public Transform spawnpoint;
+    
     private bool isMoving = true;
+    
+    private Vector3 moveDirection;
+    
+    public CharacterController controller;
+    
+    
+    
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        moveSpeed = 20F;
-        jumpForce = 25F;
-        gravityScale = 0.1F;
-
     }
 
     void Update()
-    {
+    {   
         if(isMoving == true) {
             moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, speed);
+            //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
 
+            speed += Time.deltaTime * acceleration;
 
             if(Input.GetKeyDown(KeyCode.LeftShift)) {
                 gravityScale = 0.5F;
@@ -34,12 +41,10 @@ public class PlayerController : MonoBehaviour
             if(Input.GetKeyUp(KeyCode.LeftShift)){
                 gravityScale = 0.1F;
             }
-
+            
 
             if (controller.isGrounded)
             {
-
-
                 if(Input.GetKeyDown(KeyCode.Space))
                 {
                     moveDirection.y = jumpForce;
@@ -48,27 +53,31 @@ public class PlayerController : MonoBehaviour
 
 
             moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
-            controller.Move(moveDirection * Time.deltaTime);
+            controller.Move(moveDirection * Time.deltaTime);     
 
             if(Input.GetKeyDown(KeyCode.Space)){
             // transform.position = new Vector3(0, 100, 0);
                 //it is being called everytime our capsule hits somthing 
-
-
             } 
         }
     } 
     void OnControllerColliderHit(ControllerColliderHit hit)
-           {
-               if(hit.point.z > transform.position.z + controller.radius)
-               {
-                   Death();
-               }
-           }
+           {             
+            if (hit.gameObject.tag == "Blocker") 
+            {
+                Death();
+                /*moveSpeed = 0f;
+                speed = 0f;
+                jumpForce = 0f; */
+                
+            }   
+            }
            private void Death()
-           {
-                transform.position = new Vector3(0,3.47f,0);
-               // isMoving = false;
-                //moveDirection.enabled=false;
-           }
+            {
+               transform.position = spawnpoint.transform.position;
+               /* 
+               moveSpeed = 0f;
+               speed = 0f;
+               jumpForce = 0f;*/
+            }
 }
